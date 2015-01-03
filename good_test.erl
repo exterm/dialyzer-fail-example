a3ae5a6112a84b783f5f7d17f2e182800a0b9f34
Philip Mueller <mail@philip.in-aachen.net>
2015-01-03 23:55:25 +0100

    add positive example

---
+++ good_test.erl
@@ -0,0 +1,21 @@
+-module(good_test).
+
+-export([main/0]).
+
+main() ->
+    ok = foo(bar()).
+
+foo(0) ->
+    ok;
+foo(1) ->
+    also_ok;
+foo(2) ->
+    would_be_ok_but_can_not_happen.
+
+bar() ->
+    R = io:get_line(">>"),
+    Res = case R of
+              "ok" -> 1;
+              _ -> 0
+          end,
+    Res.
